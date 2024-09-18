30bd9479d771d364ef3bcb61b00004b5945b0260
Dokyun Lim <im.koremp@gmail.com>
2024-09-18 22:00:58 +0900

    feat: add resume page with test

---
+++ src/app/resume/page.tsx
@@ -0,0 +1,28 @@
+import Link from 'next/link';
+
+import { ISite } from '@/interfaces/ISite';
+import { sites } from '@/fixtures/sites';
+
+export default function ResumePage() {
+
+  return (
+    <div>
+      <h1>Resume Page</h1>
+      <div>
+        <ul>
+          {
+            sites.map((site: ISite) => (
+              <Link href={site.href} key={site.name}>
+                <li>
+                  {site.name}
+                </li>
+              </Link>
+            ))
+
+          }
+        </ul>
+
+      </div>
+    </div>
+  );
+}
