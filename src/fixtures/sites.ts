fccbe905e74d4d3a9d5879815307ccac783fa2d0
Dokyun Lim <im.koremp@gmail.com>
2024-09-18 21:58:17 +0900

    feat: add sites fixtures

---
+++ src/fixtures/sites.ts
@@ -0,0 +1,12 @@
+import { ISite } from "@/interfaces/ISite";
+
+const sites: Array<Site> = [
+  { name: 'saramin', href: '/resume/saramin' },
+  { name: 'jobkorea', href: '/resume/jobkorea' },
+  { name: 'wanted', href: '/resume/wanted' },
+  { name: 'jumpit', href: '/resume/jumpit' },
+];
+
+export {
+  sites,
+};
