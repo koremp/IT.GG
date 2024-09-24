e77bad53eeedc8acc624be17b3e5f927f9c5f75f
Dokyun Lim <im.koremp@gmail.com>
2024-09-25 00:08:15 +0900

    feat: change project name

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 export default function HomePage() {
   return (
     <div>
-      <h1>Resume Connect</h1>
+      <h1>IT.GG</h1>
       <h2>구인구직 사이트마다 기본 이력서 양식에 맞도록 변형해주는 서비스</h2>
 
       <Link href="/resume">
