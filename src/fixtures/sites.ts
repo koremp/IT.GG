f7f4b19de40fde844bb50795bdc98a760ca7930b
Dokyun Lim <im.koremp@gmail.com>
2024-09-25 00:08:48 +0900

    refact: update href in sites

--- src/fixtures/sites.ts
+++ src/fixtures/sites.ts
@@ -1,10 +1,22 @@
 import { ISite } from "@/interfaces/ISite";
 
-const sites: Array<Site> = [
-  { name: 'saramin', href: '/resume/saramin' },
-  { name: 'jobkorea', href: '/resume/jobkorea' },
-  { name: 'wanted', href: '/resume/wanted' },
-  { name: 'jumpit', href: '/resume/jumpit' },
+const sites: Array<ISite> = [
+  {
+    name: '사람인',
+    href: 'saramin',
+  },
+  {
+    name: '잡코리아',
+    href: 'jobkorea',
+  },
+  // it resume
+  {
+    name: '원티드',
+    href: 'wanted' },
+  {
+    name: '점핏',
+    href: 'jumpit',
+  },
 ];
 
 export {
