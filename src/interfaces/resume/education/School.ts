d0b570b1316447ada6fd4283a6a8ce1f870dc5b2
Dokyun Lim <im.koremp@gmail.com>
2024-09-25 19:27:10 +0900

    feat: add exporting school types in interfaces

--- src/interfaces/resume/education/School.ts
+++ src/interfaces/resume/education/School.ts
@@ -1,10 +1,32 @@
 import { Duration } from '@/interfaces/resume/Duration';
 import { Graduations } from '@/interfaces/types/Graduations';
+import { HighSchoolMajors } from '@/interfaces/types/HighSchoolMajors';
 import { Regions } from '@/interfaces/types/Regions';
 
 export interface School {
   name: string;
   isGraduated: Graduations;
   duration: Duration;
+};
+
+export interface ElementarySchool extends School {
+  region: Regions;
+};
+
+export interface MiddleSchool extends School {
+  region: Regions;
+};
+
+export interface HighSchool extends School {
+  isTransfer: boolean;
+  major: HighSchoolMajors;
+};
+
+export interface University extends School {
+  isTransfer: boolean;
+  major: string;
+  grade?: number;
+  additionalMajor?: string;
+  isDay?: boolean; // 주간 혹은 야간
   region: Regions;
 };
