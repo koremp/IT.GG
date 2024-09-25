0ba7d53187483352fbb17cd130716fa8e06e61d0
Dokyun Lim <im.koremp@gmail.com>
2024-09-25 20:13:24 +0900

    refact: change graduations to union type

--- src/interfaces/types/Graduations.ts
+++ src/interfaces/types/Graduations.ts
@@ -1,11 +1,11 @@
-const Graduations = [
-  'Graduated', // 졸업
-  'Enrolled', // 재학
-  'LeaveOfAbsence', // 휴학
-  'Completion', // 수료
-  'LeaveHalfway', // 중퇴
-  'DropOutted', // 자퇴
-  'Graduand', // 졸업 예정
-] as const;
+const Graduations = {
+  Graduated: 'Graduated', // 졸업
+  Enrolled: 'Enrolled', // 재학
+  LeaveOfAbsence: 'LeaveOfAbsence', // 휴학
+  Completion: 'Completion', // 수료
+  LeaveHalfway: 'LeaveHalfway', // 중퇴
+  DropOutted: 'DropOutted', // 자퇴
+  Graduand: 'Graduand', // 졸업 예정
+} as const;
 
-export type Graduations = typeof Graduations[number];
+export type Graduations = typeof Graduations[keyof typeof Graduations];
