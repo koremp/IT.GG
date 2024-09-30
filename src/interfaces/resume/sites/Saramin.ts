9ad2d0a1355b8bb4c9f89811012b842c72163462
Dokyun Lim <im.koremp@gmail.com>
2024-09-30 22:42:57 +0900

    feat: add saramin base

--- src/interfaces/resume/sites/Saramin.ts
+++ src/interfaces/resume/sites/Saramin.ts
@@ -1,7 +1,68 @@
-import { Company } from "@/interfaces/resume/company";
 import { Education } from "@/interfaces/resume/education";
+import { Company } from "@/interfaces/resume/company";
+import { Experience } from "@/interfaces/resume/experience";
+
+import { Certification } from '@/interfaces/resume/license/Certification';
+import { Language } from '@/interfaces/resume/license/Language';
+import { Award } from '@/interfaces/resume/license/Award';
+
+import { HireSupport } from "../hireSupport";
+import { ArmyDuties } from "@/interfaces/types/ArmyDuty";
 
 export interface Saramin {
   education: Education;
-  companies: Array<Company>;
-}
+  experiences: Array<Experience>;
+  companies?: Array<Company>;
+  skills?: Array<string>;
+  licenses?: {
+    certifications: Array<Certification>;
+    languages: Array<Language>;
+    awards: Array<Award>;
+  };
+  files?: Array<string>; // 포트폴리오 및 기타 문서
+  experienceDesc?: string; // 경력 기술서
+  selfIntro?: {
+    name: string;
+    desc: string;
+  };
+  preference?: {
+    veteran?: string;
+    armyDuty?: ArmyDuties;
+    hireSupport?: HireSupport;
+  };
+  suggestion?: {
+
+  };
+};
+
+export type Education = '초등학교'|
+  '중학교'|
+  '초등학교'|
+  '고등학교'|
+  '대학교(2,3년)'|
+  '대학교(4년)'|
+  '대학원(석사)'|
+  '대학원(박사)';
+
+export type Suggestion = ''|
+  ''|
+  '';
+
+
+export interface SaraminURL {
+  isFreshman: boolean;
+  url: {
+    link: string;
+    name: string;
+    desc?: string;
+  };
+  education: Education;
+  experience?: Date; // when isFreshman is true, experience is not undefined
+  suggestion: Suggestion;
+
+
+};
+
+export interface SaraminFile {
+  education: Education;
+};
