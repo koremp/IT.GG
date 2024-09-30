4d2a0978847e3970afbfef753ef2a613775220b3
Dokyun Lim <im.koremp@gmail.com>
2024-10-01 04:39:11 +0900

    feat: add style and fonts, container to layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,15 @@
 import type { Metadata } from "next";
 import RecoilRootWrapper from "./RecoilRootWrapper";
 
+import { Container } from "@mui/material";
+
+import './globals.css';
+
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+
 export const metadata: Metadata = {
   title: "IT.GG",
   description: "구인구직 사이트마다 기본 이력서 양식에 맞도록 변형해주는 서비스",
@@ -15,7 +24,9 @@ export default function RootLayout({
     <html lang="ko-kr">
       <body className='antialiased'>
         <RecoilRootWrapper>
-          {children}
+          <Container sx={{ bgcolor: '#cfe8fc', minHeight: '100vh' }}>
+            {children}
+          </Container>
         </RecoilRootWrapper>
       </body>
     </html>
